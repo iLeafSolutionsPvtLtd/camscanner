e4a077373dd3d04060fe73f1125f4db5a22d02ef
Milind Leslie <mLp11@MLP11.local>
2019-04-25 19:05:25 +0530

    Refactoring code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,12 @@ import 'package:image/image.dart' as Img;
 import 'package:path_provider/path_provider.dart';
 import 'package:scan_image/scan_image.dart';
 
-enum Filter { magic, sepia, gamma, monochrome }
+enum Filter {
+  Magic,
+  Sepia,
+  Gamma,
+  Monochrome,
+}
 
 void main() => runApp(new MyApp());
 
@@ -21,7 +26,7 @@ class _MyAppState extends State<MyApp> {
   String _imagePath = 'Unknown';
 
   String _directoryPath;
-  Filter currentFilter = Filter.magic;
+  Filter currentFilter = Filter.Magic;
 
   @override
   void initState() {
@@ -60,180 +65,96 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Stack(
           children: <Widget>[
-            Positioned(
-              left: 0.0,
-              bottom: 0.0,
-              top: 0.0,
-              right: 0.0,
-              child: Container(
-                color: Colors.black,
-                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 100.0),
-                child: FutureBuilder(
-                  future: _getFilterWidget(currentFilter, false),
-                  builder: (context, AsyncSnapshot<Widget> snapShot) {
-                    if (snapShot.hasData == true) {
-                      return snapShot.data;
-                    } else {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                  },
-                ),
-              ),
-            ),
-            buildTiles()
+            _buildFilteredImage(),
+            _buildFilterTiles(),
           ],
         ),
       ),
     );
   }
 
-  Widget buildTiles() {
+  Widget _buildFilteredImage() {
+    return Positioned(
+      left: 0.0,
+      bottom: 0.0,
+      top: 0.0,
+      right: 0.0,
+      child: Container(
+        color: Colors.black,
+        padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 100.0),
+        child: FutureBuilder(
+          future: _getFilterImageWidget(currentFilter, false),
+          builder: (context, AsyncSnapshot<Widget> snapShot) {
+            if (snapShot.hasData == true) {
+              return snapShot.data;
+            } else {
+              return Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white)));
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFilterTiles() {
     return Positioned(
       left: 0.0,
       bottom: 0.0,
       right: 0.0,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                  child: Container(
-                    height: 60.0,
-                    width: 60.0,
-                    padding: EdgeInsets.all(2.0),
-                    child: FutureBuilder(
-                      future: _getFilterWidget(Filter.magic, true),
-                      builder: (context, AsyncSnapshot<Widget> snapShot) {
-                        if (snapShot.hasData == true) {
-                          return snapShot.data;
-                        } else {
-                          return Center(
-                              child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                            strokeWidth: 1.0,
-                          ));
-                        }
-                      },
-                    ),
-                  ),
-                  onTap: () {
-                    setState(() {
-                      currentFilter = Filter.magic;
-                    });
-                  },
-                ),
-                Text("Magic Filter", style: TextStyle(color: Colors.white))
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                    child: Container(
-                      height: 60.0,
-                      width: 60.0,
-                      padding: EdgeInsets.all(2.0),
-                      child: FutureBuilder(
-                        future: _getFilterWidget(Filter.sepia, true),
-                        builder: (context, AsyncSnapshot<Widget> snapShot) {
-                          if (snapShot.data != null) {
-                            return snapShot.data;
-                          } else {
-                            return Center(
-                                child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                              strokeWidth: 1.0,
-                            ));
-                          }
-                        },
-                      ),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        currentFilter = Filter.sepia;
-                      });
-                    }),
-                Text("Sepia", style: TextStyle(color: Colors.white))
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                    child: Container(
-                      height: 60.0,
-                      width: 60.0,
-                      padding: EdgeInsets.all(2.0),
-                      child: FutureBuilder(
-                        future: _getFilterWidget(Filter.gamma, true),
-                        builder: (context, AsyncSnapshot<Widget> snapShot) {
-                          if (snapShot.data != null) {
-                            return snapShot.data;
-                          } else {
-                            return Center(
-                                child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                              strokeWidth: 1.0,
-                            ));
-                          }
-                        },
-                      ),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        currentFilter = Filter.gamma;
-                      });
-                    }),
-                Text("Gamma", style: TextStyle(color: Colors.white))
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                    child: Container(
-                      height: 60.0,
-                      width: 60.0,
-                      padding: EdgeInsets.all(2.0),
-                      child: new FutureBuilder(
-                        future: _getFilterWidget(Filter.monochrome, true),
-                        builder: (context, AsyncSnapshot<Widget> snapShot) {
-                          if (snapShot.data != null) {
-                            return snapShot.data;
-                          } else {
-                            return Center(
-                                child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                              strokeWidth: 1.0,
-                            ));
-                          }
-                        },
-                      ),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        currentFilter = Filter.monochrome;
-                      });
-                    }),
-                Text("Mono Chrome", style: TextStyle(color: Colors.white))
-              ],
-            ),
-          ),
-        ],
+        children: _buildFilterTilesForFilters(Filter.values),
       ),
     );
   }
 
-  Future<Widget> _getFilterWidget(Filter filter, bool isThumbnail) async {
+  List<Widget> _buildFilterTilesForFilters(List<Filter> filters) {
+    List<Widget> filterTiles = [];
+
+    for (Filter filter in filters) {
+      Widget filterTile = Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                height: 60.0,
+                width: 60.0,
+                padding: EdgeInsets.all(2.0),
+                child: FutureBuilder(
+                  future: _getFilterImageWidget(filter, true),
+                  builder: (context, AsyncSnapshot<Widget> snapShot) {
+                    if (snapShot.hasData == true) {
+                      return snapShot.data;
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 1.0,
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  currentFilter = filter;
+                });
+              },
+            ),
+            Text(filter.toString().split('.').last, style: TextStyle(color: Colors.white))
+          ],
+        ),
+      );
+
+      filterTiles.add(filterTile);
+    }
+
+    return filterTiles;
+  }
+
+  Future<Widget> _getFilterImageWidget(Filter filter, bool isThumbnail) async {
     Map<String, String> map = {
       "imagePath": _imagePath,
       "dirPath": _directoryPath,
@@ -266,17 +187,17 @@ Widget _buildFilterImageWidget(Map<String, String> map) {
   String filePath = '$directoryPath/$extension';
 
   switch (filterName) {
-    case "Filter.magic":
+    case "Filter.Magic":
       Img.Image intermediateImage = Img.brightness(resizedCopy, -20);
       finalImage = Img.contrast(intermediateImage, 180);
       break;
-    case "Filter.sepia":
+    case "Filter.Sepia":
       finalImage = Img.sepia(resizedCopy, amount: 2.0);
       break;
-    case "Filter.gamma":
+    case "Filter.Gamma":
       finalImage = Img.adjustColor(resizedCopy, gamma: 3.0);
       break;
-    case "Filter.monochrome":
+    case "Filter.Monochrome":
       finalImage = Img.adjustColor(resizedCopy, saturation: -1.0);
       break;
   }
